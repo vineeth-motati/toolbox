70261d1900aca53ba91674d504e5cf5303781290
vineeth-motati <vineethdec06@gmail.com>
2024-06-29 18:38:53 +0530

    add pascalcase nuxt link

--- components/layout/Topbar.vue
+++ components/layout/Topbar.vue
@@ -27,14 +27,14 @@ function isActive(appId: string) {
         <div class="w-full h-full rounded-xl bg-gray-200">
             <ul class="flex flex-row items-center">
                 <li v-for="app in apps" :key="app.id" class="mr-2">
-                    <nuxt-link class="block" :to="`/${workspace}/${app.id}`">
+                    <NuxtLink class="block" :to="`/${workspace}/${app.id}`">
                         <span
                             class="inline-block py-2 px-4 hover:bg-gray-300 rounded-xl"
                             :class="{ 'bg-gray-300': isActive(app.id) }"
                         >
                             {{ app.label }}
                         </span>
-                    </nuxt-link>
+                    </NuxtLink>
                 </li>
             </ul>
         </div>
