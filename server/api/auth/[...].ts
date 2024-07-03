d7efda8789e2b2732ef90d23af0e4226b25ea154
vineeth-motati <vineethdec06@gmail.com>
2024-07-03 10:40:22 +0530

    update provider client and secret

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -5,8 +5,8 @@ export default NuxtAuthHandler({
     providers: [
         // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
         GithubProvider.default({
-            clientId: process.env.GUTHUB_CLIENT_ID,
-            clientSecret: process.env.GUTHUB_CLIENT_SECRET
+            clientId: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET
         })
     ]
 })
