cd0fa48eb3daac5b08b74b4c77da3f44c581c2f4
vineeth-motati <vineethdec06@gmail.com>
2024-07-03 12:12:59 +0530

    add auth secret

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -2,6 +2,7 @@ import GithubProvider from 'next-auth/providers/github'
 import { NuxtAuthHandler } from '#auth'
 
 export default NuxtAuthHandler({
+    secret: process.env.AUTH_SECRET,
     providers: [
         // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
         GithubProvider.default({
